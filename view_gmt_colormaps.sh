f84335304b1672ba56d35ac6e020104e0d19f727
Eric Lindsey <ericl256@gmail.com>
2020-07-03 15:05:22 +0800

    update to handle discrete colormaps and variable installation paths

--- view_gmt_colormaps.sh
+++ view_gmt_colormaps.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then 
+  echo "usage: $0 <gmt_cpts_dir>"
+  echo "script to view all available CPTs (colormaps) on this computer."
+  echo "provide the location of GMT colormap files on this computer. Probably it is /usr/local/share/cpt, or /opt/local/share/cpt, or similar."
+  exit 1
+fi
+
 gmt gmtset PS_MEDIA A0
 
-i=0
-yoffset=1.2i
+i=-1
+yoffset=-1.2i
 xoffset=0i
 ps=gmt_cmaps.ps
 
 gmt psbasemap -JX5i -R0/1/0/1 -B+n -V -P -K > $ps
 
-for cpt in `ls /opt/local/share/gmt/cpt`; do
+for cpt in `ls $1/*`; do
     i=`echo $i | awk '{print $1 + 1}'`
-    echo $i
     irem=`echo $i |awk '{print $1%9}'`
-    echo $irem
     cptbase=$(basename $cpt .cpt)
-    echo $cptbase
-    scaledcpt=$cptbase.scaled.cpt
-    echo "gmt makecpt -C$cptbase -T0/1/0.01 -D -Z > $cpt"
-    gmt makecpt -C$cptbase -T0/1/0.01 -D -Z > $scaledcpt
     if [[ $irem == 0 ]]; then
-        gmt psscale -Y-9.6i -X3.5i -J -R -C$scaledcpt -Np -Dx0/-0.4i+w3i/0.18i+h -O -K -B1:"$cpt": >> $ps
+        gmt psscale -Y9.6i -X3.5i -J -R -C$cpt -Dx0/-0.4i+w3i/0.18i+h -O -K -B1000000:"$cptbase": >> $ps
     else
-        gmt psscale -Y$yoffset -X$xoffset -J -R -C$scaledcpt -Np -Dx0/-0.4i+w3i/0.18i+h -O -K -B1:"$cpt": >> $ps
+        gmt psscale -Y$yoffset -X$xoffset -J -R -C$cpt -Dx0/-0.4i+w3i/0.18i+h -O -K -B1000000:"$cptbase": >> $ps
     fi
 done
 
